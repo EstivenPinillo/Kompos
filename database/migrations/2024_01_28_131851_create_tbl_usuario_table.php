b1103c0b04bd3a93ff8956fb2fef43353477a4ec
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-02-11 14:18:34 -0500

    feat: add migrations tbl_usuario

--- database/migrations/2024_01_28_131851_create_tbl_usuario_table.php
+++ database/migrations/2024_01_28_131851_create_tbl_usuario_table.php
@@ -12,8 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbl_usuario', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_usuario');
+            $table->unsignedBigInteger('fk_perfil');
+            $table->foreign('fk_perfil')->references('id_perfil')->on('tbl_perfil');
+            $table->unsignedBigInteger('fk_tipo_dcmnt');
+            $table->foreign('fk_tipo_dcmnt')->references('id_tipo_dcmnt')->on('tbl_tipo_documento');
+            $table->unsignedBigInteger('fk_estado');
+            $table->foreign('fk_estado')->references('id_estado')->on('tbl_estado');
+            $table->string('nombre',100);
+            $table->string('apellido',100);
+            $table->string('documento',100);
+            $table->string('correo',300);
             $table->timestamps();
+            $table->index(['id_usuario','fk_perfil','fk_tipo_dcmnt','fk_estado']);
         });
     }
 
