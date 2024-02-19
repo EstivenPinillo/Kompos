e37e94781ed092107ba3fd7d90ca767563ac676d
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-02-18 20:27:18 -0500

    feat: add migrations tbl_accesorio

--- database/migrations/2024_01_28_132623_create_tbl_accesorio_table.php
+++ database/migrations/2024_01_28_132623_create_tbl_accesorio_table.php
@@ -12,8 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbl_accesorio', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_accesorio');
+            $table->unsignedBigInteger('fk_equipo');
+            $table->foreign('fk_equipo')->references('id_equipo')->on('tbl_equipo');
+            $table->unsignedBigInteger('fk_estado');
+            $table->foreign('fk_estado')->references('id_estado')->on('tbl_estado');
+            $table->string('serial_proveedor', 150);
+            $table->string('nombre', 100);
+            $table->string('marca', 100);
+            $table->string('modelo', 150);
+            $table->string('observacion', 300);
             $table->timestamps();
+            $table->index(['id_accesorio','fk_equipo','fk_estado']);
         });
     }
 
