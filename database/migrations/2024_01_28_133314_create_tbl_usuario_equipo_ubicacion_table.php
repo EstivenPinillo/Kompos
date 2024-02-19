5209552abab3f4c09c3b7dd16b6f1bacafcb7467
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-02-18 20:47:22 -0500

    feat: add migrations tbl_usuario_equipo_ubicacion

--- database/migrations/2024_01_28_133314_create_tbl_usuario_equipo_ubicacion_table.php
+++ database/migrations/2024_01_28_133314_create_tbl_usuario_equipo_ubicacion_table.php
@@ -12,8 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbl_usuario_equipo_ubicacion', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_ueu');
+            $table->unsignedBigInteger('fk_usuario');
+            $table->foreign('fk_usuario')->references('id_usuario')->on('tbl_usuario');
+            $table->unsignedBigInteger('fk_equipo');
+            $table->foreign('fk_equipo')->references('id_equipo')->on('tbl_equipo');
+            $table->unsignedBigInteger('fk_ubicacion');
+            $table->foreign('fk_ubicacion')->references('id_ubicacion')->on('tbl_ubicacion');
+            $table->unsignedBigInteger('fk_estado');
+            $table->foreign('fk_estado')->references('id_estado')->on('tbl_estado');
             $table->timestamps();
+            $table->index(['id_ueu','fk_usuario','fk_equipo','fk_ubicacion','fk_estado']);
         });
     }
 
