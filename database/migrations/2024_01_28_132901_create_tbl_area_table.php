72aa327b19930c212c31eacc8f679056ec44d8a6
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-02-18 20:36:12 -0500

    feat: add migrations tbl_area

--- database/migrations/2024_01_28_132901_create_tbl_area_table.php
+++ database/migrations/2024_01_28_132901_create_tbl_area_table.php
@@ -12,8 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbl_area', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_area');
+            $table->unsignedBigInteger('fk_mapa');
+            $table->foreign('fk_mapa')->references('id_mapa')->on('tbl_mapa');
+            $table->unsignedBigInteger('fk_usuario');
+            $table->foreign('fk_usuario')->references('id_usuario')->on('tbl_usuario');
+            $table->unsignedBigInteger('fk_estado');
+            $table->foreign('fk_estado')->references('id_estado')->on('tbl_estado');
+            $table->string('nombre', 100);
+            $table->string('descripcion', 100);
             $table->timestamps();
+            $table->index(['id_area','fk_mapa','fk_usuario','fk_estado']);
         });
     }
 
