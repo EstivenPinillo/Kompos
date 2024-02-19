cbcaa974553c784a5cb1114e31719ad467c8d1de
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-02-18 20:17:34 -0500

    feat: add migrations tbl_servicio

--- database/migrations/2024_01_28_132402_create_tbl_servicio_table.php
+++ database/migrations/2024_01_28_132402_create_tbl_servicio_table.php
@@ -12,8 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbl_servicio', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_servicio');
+            $table->unsignedBigInteger('fk_empresa');
+            $table->foreign('fk_empresa')->references('id_empresa')->on('tbl_empresa');
+            $table->unsignedBigInteger('fk_usuario');
+            $table->foreign('fk_usuario')->references('id_usuario')->on('tbl_usuario');
+            $table->unsignedBigInteger('fk_equipo');
+            $table->foreign('fk_equipo')->references('id_equipo')->on('tbl_equipo');
+            $table->unsignedBigInteger('fk_estado');
+            $table->foreign('fk_estado')->references('id_estado')->on('tbl_estado');
             $table->timestamps();
+            $table->index(['id_servicio','fk_empresa','fk_equipo','fk_estado']);
         });
     }
 
