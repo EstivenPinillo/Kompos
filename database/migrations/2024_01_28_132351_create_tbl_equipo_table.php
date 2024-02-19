271a41d7497a18ebf4af58b051ba1cb3d8267583
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-02-18 20:13:00 -0500

    feat: add migrations tbl_equipo

--- database/migrations/2024_01_28_132351_create_tbl_equipo_table.php
+++ database/migrations/2024_01_28_132351_create_tbl_equipo_table.php
@@ -12,8 +12,23 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbl_equipo', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_equipo');
+            $table->unsignedBigInteger('fk_proveedor');
+            $table->foreign('fk_proveedor')->references('id_empresa')->on('tbl_empresa');
+            $table->unsignedBigInteger('fk_estado');
+            $table->foreign('fk_estado')->references('id_estado')->on('tbl_estado');
+            $table->string('serial_proveedor', 150);
+            $table->string('marca', 100);
+            $table->string('serial_frabrica', 150);
+            $table->string('placa_madre', 150);
+            $table->string('procesador', 150);
+            $table->string('memoria_ram', 150);
+            $table->string('almacenamiento', 100);
+            $table->string('tarjeta_grafica', 150);
+            $table->string('observacion', 300);
             $table->timestamps();
+            $table->index(['id_equipo', 'fk_proveedor', 'fk_estado']);
+
         });
     }
 
