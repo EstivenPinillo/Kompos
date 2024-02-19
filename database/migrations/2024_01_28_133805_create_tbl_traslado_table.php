c1a93a8762f57fee633422317c257f6a17832bbc
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-02-18 20:53:34 -0500

    feat: add migrations tbl_traslado

--- database/migrations/2024_01_28_133805_create_tbl_traslado_table.php
+++ database/migrations/2024_01_28_133805_create_tbl_traslado_table.php
@@ -12,8 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbl_traslado', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_traslado');
+            $table->unsignedBigInteger('fk_traslado');
+            $table->foreign('fk_traslado')->references('id_traslado')->on('tbl_traslado');
+            $table->unsignedBigInteger('fk_estado');
+            $table->foreign('fk_estado')->references('id_estado')->on('tbl_estado');
+            $table->unsignedBigInteger('fk_usuario');
+            $table->foreign('fk_usuario')->references('id_usuario')->on('tbl_usuario');
+            $table->string('acceso_remoto', 300);
             $table->timestamps();
+            $table->index(['id_traslado','fk_traslado','fk_estado','fk_usuario']);
         });
     }
 
