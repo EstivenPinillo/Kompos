211e63093e4a401f7491ad83aa0c5b3a0ceae861
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-02-18 20:18:44 -0500

    feat: add migrations tbl_reporte

--- database/migrations/2024_01_28_132451_create_tbl_reporte_table.php
+++ database/migrations/2024_01_28_132451_create_tbl_reporte_table.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbl_reporte', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_reporte');
+            $table->unsignedBigInteger('fk_usuario');
+            $table->foreign('fk_usuario')->references('id_usuario')->on('tbl_usuario');
+            $table->string('reporte', 150);
+            $table->string('descripcion', 300);
             $table->timestamps();
+            $table->index(['id_reporte','fk_usuario']);
         });
     }
 
