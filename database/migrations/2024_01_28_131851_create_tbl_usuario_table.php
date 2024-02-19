1f909afdfa980409ec3156c59c5d71d8ee5b47eb
Estiven Pinillo <estivenpinillo.software.hotmail.com>
2024-02-18 19:55:22 -0500

    feat: add migrations tbl_usuario

--- database/migrations/2024_01_28_131851_create_tbl_usuario_table.php
+++ database/migrations/2024_01_28_131851_create_tbl_usuario_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->bigIncrements('id_usuario');
             $table->unsignedBigInteger('fk_perfil');
             $table->foreign('fk_perfil')->references('id_perfil')->on('tbl_perfil');
-            $table->unsignedBigInteger('fk_tipo_dcmnt');
-            $table->foreign('fk_tipo_dcmnt')->references('id_tipo_dcmnt')->on('tbl_tipo_documento');
+            $table->unsignedBigInteger('fk_tp_dcmnt');
+            $table->foreign('fk_tp_dcmnt')->references('id_tp_dcmnt')->on('tbl_tipo_documento');
             $table->unsignedBigInteger('fk_estado');
             $table->foreign('fk_estado')->references('id_estado')->on('tbl_estado');
             $table->string('nombre',100);
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->string('documento',100);
             $table->string('correo',300);
             $table->timestamps();
-            $table->index(['id_usuario','fk_perfil','fk_tipo_dcmnt','fk_estado']);
+            $table->index(['id_usuario','fk_perfil','fk_tp_dcmnt','fk_estado']);
         });
     }
 
